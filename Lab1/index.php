6a6599ce0053748f3f1b6982ac2bf960909ce533
Gwyneth Trenchard <gwyneth.trenchard@gmail.com>
2026-01-03 12:23:56 -0500

    Add files via upload

---
+++ Lab1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab #1 -- Variables and Conditionals</title>
+  </head>
+  <body>
+    <p>Lab 1 successfully started!</p>
+    
+    <!-- Add your code below inside the body tag -->
+    <p style="color: blue;"><b>This is how to print on the browser in PHP</b></p>
+    <?php
+
+
+    $num1 = 16;
+    $num2 = 15;
+    $sum = $num1 + $num2;
+    echo "num1 is " . $num1 . " and num2 is " . $num2 . ", the sum is " . $sum;
+
+    ?>
+
+
+   
+
+  </body>
+</html>
